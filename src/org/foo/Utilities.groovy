a54e37c7ddf1902d05d255d9bb861e0d36f7b36a
pipelineascode <benjichung@yahoo.com>
2018-04-09 21:16:02 +0800

    initial commmit

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -3,6 +3,6 @@ package org.foo
 class Utilities {
   static def mvn(script, args) {
     //script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
-    println "in static mvn"
+    log.info "in static mvn"
   }
 }
