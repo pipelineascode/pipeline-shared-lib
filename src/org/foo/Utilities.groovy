bf36a1b11b551e5a45b01de6996c688e714cb95f
pipelineascode <benjichung@yahoo.com>
2018-04-09 22:09:22 +0800

    initial commmit

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -2,16 +2,18 @@ package org.foo;
 
 class Utilities {
 
+/*
 	def steps
   	Utilities(steps) {this.steps = steps}
   	def mvn(args, out) {
     	//steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
     	out.println "in static mvn"
   	}
+*/
 
-/*	
+	
 	static def mvn(script, args, out) {
     	
   	}
-*/
+
 }
