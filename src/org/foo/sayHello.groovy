f04936f7a9fc47f10087daf6aea71d4e9ae4a157
pipelineascode <benjichung@yahoo.com>
2018-04-10 21:51:11 +0800

    initial commit

---
+++ src/org/foo/sayHello.groovy
@@ -0,0 +1,7 @@
+package org.foo;
+
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
+}
