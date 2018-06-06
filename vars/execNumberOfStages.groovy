d263038ee70872eaf751372e9f78391c4c55a69e
pipelineascodecourse <benjichung@yahoo.com>
2018-06-06 22:40:53 +0800

    initial commit

--- vars/execNumberOfStages.groovy
+++ vars/execNumberOfStages.groovy
@@ -1,13 +1,16 @@
 def call(int numberOfStages) {
     pipeline {
-      agent any
-      stages {
-		for (i = 0; i <numberOfStages; i++) {
-			stage('Even Stage') {
-				steps {
-					echo "Stage ${i} is executed"
-				}
-			}	
+		agent any
+		stages 
+		{
+			for (i = 0; i <numberOfStages; i++) {
+				stage('Even Stage') 
+				{
+					steps {
+						echo "Stage ${i} is executed"
+					}
+				}	
+			}
 		}
 	}
 }
